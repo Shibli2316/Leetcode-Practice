52cde61900888722fba2b373145b31e2601d8b2a
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2024-05-13 00:18:13 +0530

    Time: 4 ms (46.62%) | Memory: 8.1 MB (5.31%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-
-
     istringstream iss(s);
     vector<string> words;
     string word;
     while (iss >> word) {
         words.push_back(word);
     }
-
-
-
-        // vector<string> splitString;
-        // int answer = 0;
-        // string store;
-        // for(int i = 0; i < s.length(); i++){
-        //     if(s[i]==' '){
-        //         splitString.push_back(store);
-        //         store="";
-        //     }
-        //     store+=s[i];
-        // }
-        for(int i = 0; i < word.size(); i++){
-            cout << word[i] << endl;
-        }
-        // return splitString[splitString.size()-1].length();
         return word.size();
-        // return 0;
     }
 };
